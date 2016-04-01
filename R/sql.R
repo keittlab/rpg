13f00821cb94caf743a18852312e0d61b5e386d3
Timothy H. Keitt <tkeitt@gmail.com>
2016-03-31 20:26:28 -0500

    Fixes

--- R/sql.R
+++ R/sql.R
@@ -2,24 +2,41 @@
 #' 
 #' Create and index on an existing table column
 #' 
-#' @param tablename the name of the table
 #' @param columnname the name of the column
+#' @param tablename the name of the table
+#' @param indexname optional index name to use
 #' @param unique if true, create a unique index
 #' @param using the index method
+#' @param collate set collation
+#' @param descending if true, sort descending
+#' @param tablespace create in this tablespace
+#' @param where restrict to rows matching predicate
 #' @param schemaname specifically in this schema
 #' 
-#' @details This function is not yet finished
+#' @details Build an index on a column.
+#' 
+#' @author Timothy H. Keitt
 #' 
 #' @export
 #' @rdname sql
-create_index = function(tablename,
-                        columnname,
+create_index = function(columnname,
+                        tablename,
+                        indexname = NULL,
                         unique = FALSE,
                         using = NULL,
+                        collate = NULL,
+                        descending = FALSE,
+                        tablespace = NULL,
+                        where = NULL,
                         schemaname = NULL){
   sp = savepoint(); on.exit(rollback(sp))
   tableschema = format_tablename(tablename, schemaname)
   prefix = if (unique) "CREATE UNIQUE INDEX" else "CREATE INDEX"
+  if (!is.null(indexname)) prefix = paste(prefix, indexname)
   if (!is.null(using)) tableschema = paste(tableschema, "USING", using)
-  execute(prefix, "ON", tableschema, "(", columnname, ")")
-  on.exit(commit(sp))}
+  sql = paste(prefix, "ON", tableschema, "(", columnname, ")")
+  if (!is.null(collate)) sql = paste(sql, "COLLATE", collate)
+  if (descending) sql = paste(sql, "DESC")
+  if (!is.null(tablespace)) sql = paste(sql, "TABLESPACE", tablespace)
+  if (!is.null(where)) sql = paste(sql, "WHERE", where)
+  execute(sql); on.exit(commit(sp))}
