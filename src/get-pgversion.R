21d1bdd9d17f31d99830046fbee70ef0cffd5cdf
Tim Keitt <tkeitt@gmail.com>
2016-05-11 09:32:06 -0500

    use system2

--- src/get-pgversion.R
+++ src/get-pgversion.R
@@ -1,6 +1,7 @@
 pg_config_path = Sys.which("pg_config")
-if (nzchar(pg_config_path)){
-  command = paste(pg_config_path, "--version")
-  version_string = system(command, TRUE)
+if (nzchar(pg_config_path))
+{
+  version_string = system2(pg_config_path, "--version", stdout = TRUE, stderr = FALSE)
   major_version = unlist(strsplit(version_string, "[ .]"))[2]
-  cat(major_version, "\n")} else cat("0\n")
+  cat(major_version, "\n")
+} else cat("0\n")
